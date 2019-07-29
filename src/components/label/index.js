32b85bf2a4521752b52e799a59eb20b0b4e99ef1
Sanyam Jain <sanyam.jain@myntra.com>
2019-07-29 15:57:24 +0530

    textfield: Don't change label color for error

--- src/components/label/index.js
+++ src/components/label/index.js
@@ -116,7 +116,7 @@ export default class Label extends PureComponent {
         outputRange: [fontSize, activeFontSize],
       }),
 
-      color,
+      color: baseColor,
     };
 
     let containerStyle = {
